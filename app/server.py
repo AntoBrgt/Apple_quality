8823187197427f526011cacc5e7c45e6d13287b8
Antonin Brengetto <antonin.brengetto@gmail.com>
2024-04-14 21:19:02 +0200

    \Add a dockerfile

---
+++ app/server.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
+
+model = joblib.load("app/modele_SVM.pkl")
+
+class_names = np.array(["good", "bad"])
+
+app = FastAPI()
+
+@app.get('/')
+
+def reed_root():
+
+    return {'message' : 'Apple model API'}
+
+@app.post('/predict')
+
+def predict(data: dict):
+    """
+    Predicts the class
+
+    Args:
+        data(dict): A dictionnary containing the features
+    """
+    features = (np.array(data['features']).reshape(1, -1))
+    prediction = model.predict(features)
+    class_name = class_names[prediction][0]
+
+    return {'predicted_class': class_name}
